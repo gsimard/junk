67f7f9f2b40806be870a3c71726329e52a807796
Guillaume Simard <guillaume.simard@cephei.ca>
2012-02-27 23:11:36 -0500

    a bit of it.

--- haskell/collatz.hs
+++ haskell/collatz.hs
@@ -1,3 +1,5 @@
+import Data.Char as C
+
 chain :: (Integral a) => a -> [a]
 chain 1 = [1]
 chain n
@@ -7,3 +9,12 @@ chain n
 numLongChains :: Int
 numLongChains = length (filter isLong (map chain [1..100]))
     where isLong xs = length xs > 15
+
+numLongChains2 :: Int
+numLongChains2 = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+sum' :: (Num a) => [a] -> a
+sum' = foldl (+) 0
+
+fromHex :: String -> Int
+fromHex s = foldl1 (\x y -> 16*x + y) $ map C.digitToInt s
